b20e4c3abe70426f3706be9f2b6960cf0f77c4ad
Benjamin Breshears <43872449+BenTheCyberOne@users.noreply.github.com>
2024-12-01 15:45:52 -0500

    Update App.js

--- hackmo/src/App.js
+++ hackmo/src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import UserDashboard from './components/UserDashboard';
-import Register from './components/Register'; // Assuming you have a Register component
+import Register from './components/Registration'; // Assuming you have a Register component
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={UserDashboard} />
-        {/* Other routes can go here */}
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+      </Routes>
       </Switch>
     </Router>
   );
