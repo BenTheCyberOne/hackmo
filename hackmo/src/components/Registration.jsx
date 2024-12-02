e38fd8c55db06b1ecf712559f5830781e9be2942
Benjamin Breshears <43872449+BenTheCyberOne@users.noreply.github.com>
2024-12-02 17:29:25 -0500

    Update Registration.jsx

--- hackmo/src/components/Registration.jsx
+++ hackmo/src/components/Registration.jsx
@@ -20,6 +20,7 @@ const Register = () => {
     try {
       const response = await fetch('/register', {
         method: 'POST',
+        credentials: 'include' // required???
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
       });
